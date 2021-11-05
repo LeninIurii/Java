6d9629d2a075a324091831d052aa22596f2451f9
LeninIurii <iurii_lenin@epam.com>
2021-11-05 23:11:53 +0300

    HW`

---
+++ compareNumbers.java
@@ -0,0 +1,20 @@
+public class compareNumbers {  
+
+    public static void main(String[] args) { 
+    
+        compareNumbers();
+    } 
+    public static void compareNumbers() {
+        int a=10;
+        int b=1;
+        int c=a+b;
+        
+        if (a >= b) {
+            System.out.println( "a >= b" );
+        } else {
+            System.out.println( "a < b");
+        }
+        
+    }
+
+ }  
---
+++ printThreeWords.java
@@ -0,0 +1,12 @@
+public class printThreeWords {  
+
+    public static void main(String[] args) { 
+    
+        printThreeWords();  
+
+    } 
+    public static void printThreeWords(){
+        System.out.println ("Orange\n"+"Banana\n"+"Apple\n");
+    }
+
+ }  
