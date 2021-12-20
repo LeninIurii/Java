9f85901200f85450b57e46aafb47b41b2c032944
LeninIurii <yur-leni@yandex.ru>
2021-12-21 00:26:44 +0300

    HW7

--- Java7/HW7.java
+++ Java7/HW7.java
@@ -1,35 +1,39 @@
 /**
 *Java 7 HW7 
 *@author Iurii Lenin
-*@version 05.12.2021
+*@version 21.12.2021
 **/
 
 
 class HW7 {
     public static void main(String[] args) {
+        int Eda = 100;
+        int plusEda = 60;
    
-        Cat[] cat = {
-            new Cat("Bars", 99, "Голоден"),
-            new Cat("Puhok", 49,"Голоден"),
-            new Cat("Nopel", 1,"Голоден"),
-            new Cat("Pops", 2,"Голоден")
-        };        
-            Plate plate = new Plate(100);
-            System.out.println(plate);
+  Cat[] cat = {
+        new Cat("Bars", 60, "Голоден"),
+        new Cat("Puhok", 49,"Голоден"),
+        new Cat("Nopel", 25,"Голоден"),
+        new Cat("Pops", 21,"Голоден")
+    };
+        Plate plate = new Plate(Eda);
+        System.out.println("Еды в тарелке до того как коты поели \n" + plate);
         
-            for(Cat catamo : cat) {
-                catamo.eat(plate);
-                System.out.println(catamo);
-            };
-            System.out.println("Осталось еды после того как коты поели\n" + plate);
-            plate.setFood(50);
-            System.out.println("Добавили еды для новых котков:" + plate.getFood() + "\n Колличество еды в тарелке для новых котят\n" + plate);
+        for(Cat catamo : cat) {
+             catamo.eat(plate);
+            System.out.println(catamo);
+        };
+        
+        System.out.println("Еды в тарелке после того как коты поели \n" + plate);
+        plate.setFood(plusEda);
+        System.out.println("Добавим " + plusEda + " еды в тарелку \n" + "Колличество еды посе добавления \n" + plate);
+    
     }
 }
 
 class Cat {
-    private String name;
-    private int appetite; 
+   private String name;
+   private int appetite; 
     private String fullnes;
     
     Cat(String name, int appetite, String fullnes) {
@@ -38,55 +42,56 @@ class Cat {
         this.fullnes = fullnes;
     }
     
-        public int getAppetit() {
+   public int getAppetit() {
         return appetite;
-        }
+    }
     
-        public String getFullnes() {
-            return fullnes;
-        }
+    public String getFullnes() {
+        return fullnes;
+    }
     
-        public void setFullnes(String fullnes) {
-            this.fullnes = fullnes;
-        }
+    public void setFullnes(String fullnes) {
+        this.fullnes = fullnes;
+ 
+    }
     
-        void eat(Plate plate) {
-             int c = ((plate.getFood()) - (getAppetit()));
-                if( c >= 0) {
-                plate.decreaseFood(appetite);
-                setFullnes("Поел!");     
+     void eat(Plate plate) {    
+          int c = ((plate.getFood()) - (getAppetit()));
+           if( c > 0) {
+        plate.decreaseFood(appetite);
+             setFullnes("Поел");      
            } else setFullnes("Все ещё худыш и хочет есть");
-        }
+    }
     
-        @Override
-            public String toString() {
-            return "Cat: " + name + ", apetit: " + appetite + ", fullnes: " + fullnes;
-        }
+    @Override
+    public String toString() {
+        return "Cat: " + name + ", apetit: " + appetite + ", fullnes: " + fullnes;
+    }
 }
 
 class Plate {
-    private int food;
-    
+   private int food;
+
     Plate(int food) {
         this.food = food;
     }
 
-        public int getFood() {
-            return food;
-        }
-        
-        public void setFood(int food) {
-            this.food = food;
-        }
-   
-        void  decreaseFood(int food) {
-            if(getFood() > 0) {
-                this.food -= food;
-            }
-        }
+  public int getFood() {
+        return food;
+   }
+
+    public void setFood(int food) {
+       this.food += food;
+    }
+
+    void  decreaseFood(int food) {  
+         if(getFood() > 0) {
+         this.food -= food;
+         }
+    }
     
-        @Override
-            public String toString() {
-                return "Plate: " + food;
-            }
+    @Override
+    public String toString() {
+        return " Plate:" + food;
+    }
 }
